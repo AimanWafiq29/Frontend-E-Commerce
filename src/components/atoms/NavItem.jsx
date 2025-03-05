65c4939d29bdfe929e09e260976a80c98bfa5b37
Aiman Wafiq <aimanwafiq341@gmail.com>
2025-03-05 12:23:51 +0800

    penambahan is active pada menu nav

--- src/components/atoms/NavItem.jsx
+++ src/components/atoms/NavItem.jsx
@@ -3,9 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const NavItem = ({ to, label }) => {
   return (
-    <NavLink to={to} className="flex flex-col items-center text-gray-700 hover:text-black">
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `flex flex-col items-center text-gray-700 hover:text-black ${
+          isActive ? "text-black font-semibold border-b-2 border-black" : ""
+        }`
+      }
+    >
       {label}
-      <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
     </NavLink>
   );
 };
