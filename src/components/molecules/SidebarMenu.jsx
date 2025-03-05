8eae9c6dd171d777614117466261391bced29fe6
Aiman Wafiq <aimanwafiq341@gmail.com>
2025-03-05 13:02:31 +0800

    Penambahan Sidebar menu mobile view

---
+++ src/components/molecules/SidebarMenu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import IconButton from "../atoms/IconButton";
+import DropdownIcon from "../../assets/dropdown_icon.png";
+
+const SidebarMenu = ({ visible, setVisible }) => {
+    return (
+        <div className={`fixed top-0 right-0 bottom-0 bg-white transition-all duration-300 ${visible ? "w-full" : "w-0 overflow-hidden"}`}>
+            <div className="flex flex-col text-gray-600">
+                <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+                    <IconButton icon={DropdownIcon} alt="Back" className="h-4 rotate-180" />
+                    <p>Back</p>
+                </div>
+                <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-300" to="/">HOME</NavLink>
+                <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-300" to="/collection">COLLECTION</NavLink>
+                <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-300" to="/about">ABOUT</NavLink>
+                <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b border-gray-300" to="/contact">CONTACT</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarMenu;
--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../atoms/Logo";
 import IconButton from "../atoms/IconButton";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import CartIcon from "../../assets/cart_icon.png";
 import SearchIcon from "../../assets/search_icon.png";
+import MenuIcon from "../../assets/menu_icon.png";
+import DropdownIcon from "../../assets/dropdown_icon.png";
 import NavMenu from "../molecules/Navmenu";
 import ProfileDropdown from "../molecules/ProfileDropdown";
+import NavItem from "../atoms/NavItem";
+import SidebarMenu from "../molecules/SidebarMenu";
 
 const Navbar = () => {
-  return (
-    <div className="flex items-center justify-between py-5 px-4 bg-amber-600">
-      <Logo />
-      <NavMenu />
-      <div className="flex items-center gap-6">
-        <IconButton icon={SearchIcon} alt="Search" />
-        <ProfileDropdown />
-        <Link to="/cart" className="relative">
-          <IconButton icon={CartIcon} alt="Cart" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full text-[8px]">
-            3
-          </p>
-        </Link>
-      </div>
-    </div>
-  );
+
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <div className="flex items-center justify-between py-5 px-4">
+            <Logo />
+            <NavMenu />
+            <div className="flex items-center gap-6">
+                <IconButton icon={SearchIcon} alt="Search" />
+                <ProfileDropdown />
+                <Link to="/cart" className="relative">
+                    <IconButton icon={CartIcon} alt="Cart" />
+                    <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full text-[8px]">
+                        3
+                    </p>
+                </Link>
+                <IconButton onClick={() => setVisible(true)} icon={MenuIcon} alt="Menu" className="w-5 sm:hidden" />
+            </div>
+
+            {/* Sidebar Menu */}
+            <SidebarMenu visible={visible} setVisible={setVisible} />
+
+        </div>
+    );
 };
 
 export default Navbar;
